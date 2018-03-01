5d90cf51f2a4db83e1152e110b7407b43aaba67e
Eduardo Antuña <eduadiez@gmail.com>
2018-03-01 16:37:27 +0100

    Initial work

---
+++ build/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const http = require('http');
+const ens = require('./ens');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+
+const IPFS_REDIRECT = process.env.IPFS_REDIRECT || "http://ipfs:8080";
+const STANDALONE = process.env.STANDALONE || false;
+
+http.createServer(async (req, res) => {
+    const { headers, method, url } = req;
+    console.log("#### headers ###");
+    console.log(req.headers);
+    console.log("#### method ###");
+    console.log(req.method);
+    console.log("#### url ###");
+    console.log(req.url);
+
+    var domain = req.headers.host;
+
+    if (STANDALONE) {
+        if (req.url.endsWith('.eth')) {
+            domain = req.url.slice(1);
+            req.url=""
+        }
+    }
+
+    const content = await ens.getContent(domain);
+    if (content != "0x0000000000000000000000000000000000000000000000000000000000000000") {
+        const url = IPFS_REDIRECT + content;
+        console.log(url);
+        proxy.web(req, res, {
+            target: url,
+        });
+    }else if (STANDALONE){
+        console.log(IPFS_REDIRECT);
+        proxy.web(req, res, {
+            target: IPFS_REDIRECT,
+        });
+    }
+}).listen(80);
