a3773aeae891d595be1870e2b7d110553395a595
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-06-15 10:05:21 +0200

    added unsynced.html response

--- build/src/ens.js
+++ build/src/ens.js
@@ -1,6 +1,6 @@
 var Web3 = require('web3');
 
-const WEB3HOSTWS = process.env.WEB3HOSTWS || "ws://my.ethchain.dnp.dappnode.eth:8546" ;
+const WEB3HOSTWS = "ws://my.ethchain.dnp.dappnode.eth:8546";
 
 var web3 = new Web3(WEB3HOSTWS);
 
@@ -8,9 +8,9 @@ const ensAbi = [{ "constant": true, "inputs": [{ "name": "node", "type": "bytes3
 const ensAddr = '0x314159265dd8dbb310642f98f50c066173c1259b'
 const resolverAbi = [{ "constant": true, "inputs": [{ "name": "interfaceID", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "key", "type": "string" }, { "name": "value", "type": "string" }], "name": "setText", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "contentTypes", "type": "uint256" }], "name": "ABI", "outputs": [{ "name": "contentType", "type": "uint256" }, { "name": "data", "type": "bytes" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "x", "type": "bytes32" }, { "name": "y", "type": "bytes32" }], "name": "setPubkey", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }], "name": "content", "outputs": [{ "name": "ret", "type": "bytes32" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }], "name": "addr", "outputs": [{ "name": "ret", "type": "address" }], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "key", "type": "string" }], "name": "text", "outputs": [{ "name": "ret", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "contentType", "type": "uint256" }, { "name": "data", "type": "bytes" }], "name": "setABI", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }], "name": "name", "outputs": [{ "name": "ret", "type": "string" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "name", "type": "string" }], "name": "setName", "outputs": [], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "hash", "type": "bytes32" }], "name": "setContent", "outputs": [], "payable": false, "type": "function" }, { "constant": true, "inputs": [{ "name": "node", "type": "bytes32" }], "name": "pubkey", "outputs": [{ "name": "x", "type": "bytes32" }, { "name": "y", "type": "bytes32" }], "payable": false, "type": "function" }, { "constant": false, "inputs": [{ "name": "node", "type": "bytes32" }, { "name": "addr", "type": "address" }], "name": "setAddr", "outputs": [], "payable": false, "type": "function" }, { "inputs": [{ "name": "ensAddr", "type": "address" }], "payable": false, "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": false, "name": "a", "type": "address" }], "name": "AddrChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": false, "name": "hash", "type": "bytes32" }], "name": "ContentChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": false, "name": "name", "type": "string" }], "name": "NameChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": true, "name": "contentType", "type": "uint256" }], "name": "ABIChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": false, "name": "x", "type": "bytes32" }, { "indexed": false, "name": "y", "type": "bytes32" }], "name": "PubkeyChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "node", "type": "bytes32" }, { "indexed": true, "name": "indexedKey", "type": "string" }, { "indexed": false, "name": "key", "type": "string" }], "name": "TextChanged", "type": "event" }];
 
-setInterval(function () {
+setInterval(function() {
     web3.eth.net.isListening().then().catch(e => {
-        console.log('[ - ] Lost connection to the node: '+ WEB3HOSTWS +', reconnecting');
+        console.log('[ - ] Lost connection to the node: ' + WEB3HOSTWS + ', reconnecting');
         web3.setProvider(WEB3HOSTWS);
     })
 }, 10000)
@@ -31,14 +31,20 @@ exports.getContent = async (name) => {
     const node = namehash(name);
     console.log(node);
 
-    var ens = new web3.eth.Contract(ensAbi,ensAddr );
+    var ens = new web3.eth.Contract(ensAbi, ensAddr);
 
-    const resolverAddress = await ens.methods.resolver(node).call();
-    console.log("Addr: " + resolverAddress);
-    if (resolverAddress === '0x0000000000000000000000000000000000000000') {
-        return "0x0000000000000000000000000000000000000000000000000000000000000000";
+    try {
+        const resolverAddress = await ens.methods.resolver(node).call();
+        console.log("Addr: " + resolverAddress);
+        if (resolverAddress === '0x0000000000000000000000000000000000000000') {
+            return "0x0000000000000000000000000000000000000000000000000000000000000000";
+        }
+    } catch (e) {
+        console.log(e)
+        return "0x"
     }
     const resolver = new web3.eth.Contract(resolverAbi, resolverAddress);
     const content = await resolver.methods.text(node, "dnslink").call();
     return content;
+
 }
--- build/src/index.js
+++ build/src/index.js
@@ -3,33 +3,33 @@ require('dotenv').config()
 const http = require('http');
 const ens = require('./ens');
 const httpProxy = require('http-proxy');
+const defaultResponse = 'unsynced.html'
 
 const proxy = httpProxy.createProxyServer({});
 
-const IPFS_REDIRECT = process.env.IPFS_REDIRECT || "http://my.ipfs.dnp.dappnode.eth:8080";
-const STANDALONE = process.env.STANDALONE || false;
+const IPFS_REDIRECT = "http://my.ipfs.dnp.dappnode.eth:8080";
 
 http.createServer(async (req, res) => {
     var domain = req.headers.host;
 
-    if (STANDALONE) {
-        if (req.url.endsWith('.eth')) {
-            domain = req.url.slice(1);
-            req.url=""
-        }
-    }
-
     const content = await ens.getContent(domain);
-    if (content != "0x0000000000000000000000000000000000000000000000000000000000000000") {
+    if (content == "0x") {
+        fs.readFile(defaultResponse, "binary", function(err, file) {
+            if (err) {
+                response.writeHead(500, { "Content-Type": "text/plain" });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.createReadStream(defaultResponse).pipe(response)
+            response.end();
+        });
+    }else if (content != "0x0000000000000000000000000000000000000000000000000000000000000000") {
         const url = IPFS_REDIRECT + content;
         console.log(url);
         proxy.web(req, res, {
             target: url,
-        });
-    }else if (STANDALONE){
-        console.log(IPFS_REDIRECT);
-        proxy.web(req, res, {
-            target: IPFS_REDIRECT,
         });
     }
 }).listen(80);
