ec811c412d2d250e36655e45f537d6a0d19368d5
Eduardo Antuña Díez <eduadiez@gmail.com>
2018-06-15 17:03:50 +0200

    added 404.html
    * ens.js improved
    * index.js improved

--- build/src/index.js
+++ build/src/index.js
@@ -3,29 +3,24 @@ require('dotenv').config()
 const http = require('http');
 const ens = require('./ens');
 const httpProxy = require('http-proxy');
-const defaultResponse = 'unsynced.html'
+const responseUnsynced = 'unsynced.html'
+const response404 = '404.html'
+const fs = require('fs');
+const IPFS_REDIRECT = "http://my.ipfs.dnp.dappnode.eth:8080";
 
 const proxy = httpProxy.createProxyServer({});
 
-const IPFS_REDIRECT = "http://my.ipfs.dnp.dappnode.eth:8080";
-
 http.createServer(async (req, res) => {
     var domain = req.headers.host;
-
     const content = await ens.getContent(domain);
-    if (content == "0x") {
-        fs.readFile(defaultResponse, "binary", function(err, file) {
-            if (err) {
-                response.writeHead(500, { "Content-Type": "text/plain" });
-                response.write(err + "\n");
-                response.end();
-                return;
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            fs.createReadStream(defaultResponse).pipe(response)
-            response.end();
-        });
-    }else if (content != "0x0000000000000000000000000000000000000000000000000000000000000000") {
+    
+    if (content == "0x404") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(response404).pipe(res)
+    } else if (content == "0x") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(responseUnsynced).pipe(res)
+    } else {
         const url = IPFS_REDIRECT + content;
         console.log(url);
         proxy.web(req, res, {
