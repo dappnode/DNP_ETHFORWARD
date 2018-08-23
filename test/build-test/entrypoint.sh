24ad8e62cb03ebcacf63a131ec68def3d0dee8f8
Unknown <guillem.lion.benosa@gmail.com>
2018-08-23 19:00:33 +0200

    fix typo

--- test/build-test/entrypoint.sh
+++ test/build-test/entrypoint.sh
@@ -4,36 +4,33 @@ echo " "
 echo " Resolving decentral.eth"
 echo " "
 
-RES=$(curl "http://decentral.eth/")
-echo "${RES}" | awk '/<title>Decentralized/{exit 0} !/<title>Decentralized/{exit 1}' ;
-    if [ "$?" = "0" ]
-    then
-        echo "Result contains expected string"
-    else
-        echo "Result DOES NOT contain expected string"
-        echo "Expected HTML containing <title>Decentralized Portal</title>"
-        echo "Received:  ${RES}"
-        exit 1
-    fi
+RES1=$(curl "http://decentral.eth/")
+echo "${RES1}" | awk '/<title>Decentralized/{exit 0} !/<title>Decentralized/{exit 1}' ;
+if [ "$?" = "0" ]
+then
+    echo "Result contains expected string"
+else
+    echo "Result DOES NOT contain expected string"
+    echo "Expected HTML containing <title>Decentralized Portal</title>"
+    echo "Received:  ${RES1}"
+    exit 1
 fi
 
 echo " "
 echo " Resolving decentralFake.eth"
 echo " "
 
-RES=$(curl "http://decentralFake.eth/")
-echo "${RES}" | awk '/<title>Decentralized/{exit 0} !/<title>Decentralized/{exit 1}' ;
-    if [ "$?" = "0" ]
-    then
-        echo "Result contains expected string"
-    else
-        echo "Result DOES NOT contain expected string"
-        echo "Expected HTML containing <title>Decentralized Portal</title>"
-        echo "Received:  ${RES}"
-        exit 1
-    fi
+RES2=$(curl "http://decentralFake.eth/")
+echo "${RES2}" | awk '/<title>Decentralized/{exit 0} !/<title>Decentralized/{exit 1}' ;
+if [ "$?" = "0" ]
+then
+    echo "Result contains expected string"
+else
+    echo "Result DOES NOT contain expected string"
+    echo "Expected HTML containing <title>Decentralized Portal</title>"
+    echo "Received:  ${RES2}"
+    exit 1
 fi
-
 
 # check_domain my.ethchain.dnp.dappnode.eth 172.33.1.6
 # check_domain ethchain.dappnode.eth 172.33.1.3
