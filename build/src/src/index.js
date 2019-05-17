7c8b0c9d0603e9b6ad990b17a4a1e0edebed7e78
dapplion <guillem.lion.benosa@gmail.com>
2019-05-17 22:03:38 +0200

    Add ENV to pin content on visit

--- build/src/src/index.js
+++ build/src/src/index.js
@@ -3,6 +3,7 @@ const ens = require("./ens");
 const httpProxy = require("http-proxy");
 const fs = require("fs");
 const path = require("path");
+const httpGet = require("./utils/httpGet");
 
 // Define params
 
@@ -10,9 +11,11 @@ const responseUnsynced = path.join(__dirname, "unsynced.html");
 const response404 = path.join(__dirname, "404.html");
 const IPFS_REDIRECT =
   process.env.IPFS_REDIRECT || "http://my.ipfs.dnp.dappnode.eth:8080";
-const SWARM_REDIRECT =
-  process.env.SWARM_REDIRECT || "http://swarm.dappnode";
+const SWARM_REDIRECT = process.env.SWARM_REDIRECT || "http://swarm.dappnode";
 const port = process.env.DEV_PORT || 80;
+
+const pinContentOnVisit = process.env.PIN_CONTENT_ON_VISIT;
+const IPFS_API = "http://my.ipfs.dnp.dappnode.eth:5001/api/v0";
 
 // Start server
 
@@ -34,13 +37,18 @@ http
       res.writeHead(200, { "Content-Type": "text/html" });
       fs.createReadStream(responseUnsynced).pipe(res);
     } else {
-      if (content.startsWith("/ipfs/")){
+      if (content.startsWith("/ipfs/")) {
         const url = IPFS_REDIRECT + content;
         console.log(`Proxying url: ${url}`);
         proxy.web(req, res, {
           target: url
         });
-      } else if(content.startsWith("/bzz:/")){
+        // If the user has requested so, pin the content when visiting it
+        if (pinContentOnVisit)
+          httpGet(`${IPFS_API}/pin/add?arg=${content}`)
+            .then(res => console.log(`Pinned ${content}: ${res}`))
+            .catch(e => console.log(`Error pinning ${content}: ${e.message}`));
+      } else if (content.startsWith("/bzz:/")) {
         const url = SWARM_REDIRECT + content;
         console.log(`Proxying url: ${url}`);
         proxy.web(req, res, {
---
+++ build/src/test/utils/httpGet.test.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+const httpGet = require("../../src/utils/httpGet");
+
+/**
+ * Test the custom http GET request util
+ */
+
+const url = "http://jsonplaceholder.typicode.com/todos/1";
+
+describe("util > httpGet", () => {
+  it("should retrieve data from a sample API", async () => {
+    const res = await httpGet(url);
+    assert.deepEqual(
+      JSON.parse(res),
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false
+      },
+      "Wrong response"
+    );
+  });
+
+  it("should return error for a broken url", async () => {
+    const error = await httpGet("http://fakeurl.fake").catch(e => e.message);
+    assert.equal(
+      error,
+      "getaddrinfo ENOTFOUND fakeurl.fake fakeurl.fake:80",
+      "Wrong error message"
+    );
+  });
+});
