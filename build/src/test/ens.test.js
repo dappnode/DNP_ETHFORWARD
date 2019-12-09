23b2da90f7bde608541d0888b528ed8f9f2e76b4
dapplion <guillem.lion.benosa@gmail.com>
2019-12-09 19:36:59 +0100

    Update tests

--- build/src/test/ens.test.js
+++ build/src/test/ens.test.js
@@ -15,19 +15,18 @@ const ensDomains = {
   "my.admin.dnp.dappnode.eth": "0x404",
   "mycrypto.dappnode.eth":
     "/ipfs/Qmdojo8KAsZu7XTkETYwSiZMCjdUa58YNZCUKmsZ21i8gV",
-  "theswarm.eth": "/bzz:/7027b30fa1702e5badb0d5a0378e01566da7798c9b2bf054b7e1f3168480ef96",
+  "theswarm.eth": "/bzz:/[0-9a-fA-F]{64}", // Use regex for swarm since it changes often
   "eth2dai.eduadiez.eth": "/ipfs/QmZoHo1wi4G9VHX6xLmMBRdFpdHMkHnsqVXqV6Vsng9m8j"
 };
 
-const timeout = 10 * 1000;
-
-describe("ens", () => {
-  describe("ens.getContent", () => {
-    for (const [domain, expectedContent] of Object.entries(ensDomains)) {
-      it(`should return the IPFS hash of ${domain}`, async () => {
-        const content = await ens.getContent(domain);
-        assert.equal(content, expectedContent, "Wrong content");
-      }).timeout(timeout);
-    }
-  });
+describe("ens.getContent", () => {
+  for (const [domain, contentRegex] of Object.entries(ensDomains)) {
+    it(`should return the IPFS hash of ${domain}`, async () => {
+      const content = await ens.getContent(domain);
+      assert.ok(
+        RegExp(contentRegex).test(content),
+        `Expected:\n  ${content}  \nto match:\n  ${contentRegex}`
+      );
+    }).timeout(10 * 1000);
+  }
 });
